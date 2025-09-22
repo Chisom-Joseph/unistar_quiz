31d880bbe9d890927cea791f61a57b2b992ad07f
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-09-22 16:15:09 +0100

    refactor: extract header into partial and remove duplicate layout code
    - Moved header markup from `home.php` and `layouts/main.php` into `views/partials/header.php`
    - Updated `home.php` to require the new partial
    - Removed redundant layout HTML from `layouts/main.php`

---
+++ views/partials/header.php
@@ -0,0 +1,166 @@
+<?php
+// views/layouts/main.php
+require_once 'config/constants.php';
+require_once 'includes/auth.php';
+require_once 'classes/User.php';
+
+$user = new User();
+$isLoggedIn = isset($_SESSION['user_id']);
+$userData = $isLoggedIn ? $user->getUserData($_SESSION['user_id']) : null;
+$isAdmin = $isLoggedIn && $userData['role'] === 'admin';
+$currentPage = isset($_GET['page']) ? $_GET['page'] : ''; 
+?>
+<!doctype html>
+<html class="no-js" lang="zxx">
+<head>
+   <meta charset="utf-8">
+   <meta http-equiv="x-ua-compatible" content="ie=edge">
+   <title>Educate - University, Online Courses, School & Education Template</title>
+   <meta name="description" content="">
+   <meta name="viewport" content="width=device-width, initial-scale=1"> 
+
+   <!-- Place favicon.ico in the root directory -->
+   <link rel="shortcut icon" type="image/x-icon" href="/public/img/logo/favicon.png">
+
+   <!-- CSS here -->
+   <link rel="stylesheet" href="/public/css/bootstrap.min.css">
+   <link rel="stylesheet" href="/public/css/animate.css">
+   <link rel="stylesheet" href="/public/css/custom-animation.css">
+   <link rel="stylesheet" href="/public/css/slick.css">
+   <link rel="stylesheet" href="/public/css/nice-select.css">
+   <link rel="stylesheet" href="/public/css/flaticon_xoft.css">
+   <link rel="stylesheet" href="/public/css/swiper-bundle.css">
+   <link rel="stylesheet" href="/public/css/meanmenu.css">
+   <link rel="stylesheet" href="/public/css/font-awesome-pro.css">
+   <link rel="stylesheet" href="/public/css/magnific-popup.css">
+   <link rel="stylesheet" href="/public/css/spacing.css">
+   <link rel="stylesheet" href="/public/css/main.css">
+</head>
+
+<body>
+
+   <!-- preloader -->
+   <div id="preloader">
+      <div class="preloader">
+         <span></span>
+         <span></span>
+      </div>
+   </div>
+   <!-- preloader end  -->
+
+   <!-- back-to-top-start  -->
+   <button class="scroll-top scroll-to-target" data-target="html">
+      <i class="far fa-angle-double-up"></i>
+   </button>
+   <!-- back-to-top-end  -->
+
+   <!-- it-offcanvus-area-start -->
+   <div class="it-offcanvas-area">
+      <div class="itoffcanvas">
+         <div class="it-offcanva-bottom-shape d-none d-xxl-block">
+         </div>
+         <div class="itoffcanvas__close-btn">
+            <button class="close-btn"><i class="fal fa-times"></i></button>
+         </div>
+         <div class="itoffcanvas__logo">
+            <a href="/">
+               <img src="/public/img/logo/logo-white.png" alt="">
+            </a>
+         </div>
+         <div class="itoffcanvas__text">
+            <p>Suspendisse interdum consectetur libero id. Fermentum leo vel orci porta non. Euismod viverra nibh
+               cras pulvinar suspen.</p>
+         </div>
+         <div class="it-menu-mobile"></div>
+         <div class="itoffcanvas__info">
+            <h3 class="offcanva-title">Get In Touch</h3>
+            <div class="it-info-wrapper mb-20 d-flex align-items-center">
+               <div class="itoffcanvas__info-icon">
+                  <a href="#"><i class="fal fa-envelope"></i></a>
+               </div>
+               <div class="itoffcanvas__info-address">
+                  <span>Email</span>
+                  <a href="maito:hello@yourmail.com">hello@yourmail.com</a>
+               </div>
+            </div>
+            <div class="it-info-wrapper mb-20 d-flex align-items-center">
+               <div class="itoffcanvas__info-icon">
+                  <a href="#"><i class="fal fa-phone-alt"></i></a>
+               </div>
+               <div class="itoffcanvas__info-address">
+                  <span>Phone</span>
+                  <a href="tel:(00)45611227890">(00) 456 1122 7890</a>
+               </div>
+            </div>
+            <div class="it-info-wrapper mb-20 d-flex align-items-center">
+               <div class="itoffcanvas__info-icon">
+                  <a href="#"><i class="fas fa-map-marker-alt"></i></a>
+               </div>
+               <div class="itoffcanvas__info-address">
+                  <span>Location</span>
+                  <a href="htits://www.google.com/maps/@37.4801311,22.8928877,3z" target="_blank">Riverside 255,
+                     San Francisco, USA </a>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+   <div class="body-overlay d-none"></div>
+   <!-- it-offcanvus-area-end -->
+
+   <header class="it-header-height">
+
+
+   
+         <!-- header-area-start -->
+         <div id="header-sticky" class="it-header-5-area">
+            <div class="container">
+               <div class="it-header-wrap p-relative">
+                  <div class="row align-items-center">
+                     <div class="col-xl-2 col-6">
+                        <div class="it-header-5-logo">
+                           <a href="/"><img src="/public/img/logo/logo-black.png" alt=""></a>
+                        </div>
+                     </div>
+                     <div class="col-xl-7 d-none d-xl-block">
+                        <div class="it-header-2-main-menu">
+                           <nav class="it-menu-content">
+                              <ul>
+                                 <li><a href="/">Home</a></li>
+                                 <li><a href="/?page=about">about us</a></li>
+                                 <li><a href="/?page=contact">contact</a></li>
+                                 <li><a href="/?page=register">Register</a></li>
+                                 <li><a href="/?page=login">Login</a></li>
+                              </ul>
+                           </nav>
+                        </div>
+                     </div>
+                     <div class="col-xl-3 col-6">
+                        <div class="it-header-2-right d-flex align-items-center justify-content-end">
+                           <div class="it-header-2-button d-none d-md-block">
+                              <a class="it-btn" href="/?page=contact">
+                                 <span>
+                                    Contact Us
+                                    <svg width="17" height="14" viewBox="0 0 17 14" fill="none"
+                                       xmlns="http://www.w3.org/2000/svg">
+                                       <path d="M11 1.24023L16 7.24023L11 13.2402" stroke="currentcolor"
+                                          stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round"
+                                          stroke-linejoin="round" />
+                                       <path d="M1 7.24023H16" stroke="currentcolor" stroke-width="1.5"
+                                          stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                 </span>
+                              </a>
+                           </div>
+                           <div class="it-header-2-bar d-xl-none">
+                              <button class="it-menu-bar"><i class="fa-solid fa-bars"></i></button>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- header-area-end -->
+
+   </header>
